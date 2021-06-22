9bc04599435fb55e253b7161ca8b09ebec5fff95
tunguyen <tunguyen060811@gmail.com>
2021-06-22 23:57:52 +0700

    working with danamic search

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,8 +1,28 @@
 <template>
     <div class="books">
-        <div class="book-row" v-for="(booksRow, index) in books" :key="index">
-            <Book v-for="book in booksRow" :key="book.id" :book="book"/>
+        <div class="search">
+            <v-text-field
+                label="Search"
+                hide-details="auto"
+                :dark="true"
+                v-model="searchKey"
+                @keyup="searchBook()"
+            ></v-text-field>
+            <div class="search-result" id="search-result">
+
+            </div>
         </div>
+        <div v-if="!haveSearchResult">
+            <div class="books-row" id="books-row" v-for="(booksRow, index) in books" :key="index">
+                <Book v-for="book in booksRow" :key="book.id" :book="book"/>
+            </div>
+        </div>
+        <div v-else>
+            <div class="books-search" id="books-search">
+                <Book v-for="book in booksSearchResult" :key="book.id" :book="book"/>
+            </div>
+        </div>
+        
     </div>
 </template>
 
@@ -17,7 +37,10 @@ export default Vue.extend({
 
     data: () => ({
         books: [],
-        booksApi: "http://localhost:3000/books"
+        booksApi: "http://localhost:3000/books",
+        searchKey: '',
+        booksSearchResult: [],
+        haveSearchResult: false
     }),
 
     async created() {
@@ -29,17 +52,78 @@ export default Vue.extend({
             let booksRespone = await fetch(this.booksApi)
             let books = await booksRespone.json()
             return books
+        },
+        searchBook(){
+            let searchResult = document.getElementById('search-result')
+            if (!this.searchKey.length) {
+                if (searchResult) {
+                    searchResult.style.display = 'none'
+                    searchResult.innerHTML = ''
+                    this.booksSearchResult = []
+                    this.haveSearchResult = false
+                }
+            }else {
+                if (searchResult) {
+                    searchResult.style.display = 'block'
+                }
+
+                for (const [row, booksOfRow] of Object.entries(this.books)) {
+                    console.log(row)
+                    for (let book of booksOfRow) {
+                        let bookName = book.name
+                        if (bookName.includes(this.searchKey)){
+                            let h3 = document.createElement("h3")
+                            h3.innerHTML = bookName
+                            searchResult?.appendChild(h3)
+                            
+                            let isBookInBooksSearch = this.isBookInBooksSearch(book, this.booksSearchResult)
+                            if (!isBookInBooksSearch) {
+                                this.booksSearchResult.push(book)
+                            }
+                            if (this.booksSearchResult) {
+                                this.haveSearchResult = true
+                            }
+                        }
+                    }
+                }
+            }
+        },
+
+        isBookInBooksSearch(book: object, booksSearchResult: Array<object>) {
+            for (let bookSearch of booksSearchResult) {
+                if (book.id === bookSearch.id) {
+                    return true
+                }
+            }
+            return false
         }
     }
 })
 </script>
 
 <style scoped>
-.book-row {
+.books-row,
+.books-search {
     display: flex;
     align-items: center;
     justify-content: space-around;
-    margin-bottom: 3%;
+    margin: 3% 0;
     text-align: center;
+}
+
+.book-search {
+    display: none;
+}
+
+.search {
+    width: 70%;
+    margin: auto;
+}
+
+.search-result {
+    text-align: left;
+    background: white;
+    display: none;
+    opacity: 0.5;
 }
 </style>
